8a14d5e426503c854258e9dd5e207317f8d99a95
Phạm Đắc Dy <153878458+phamdacdy@users.noreply.github.com>
2024-07-28 16:37:51 +0700

    Section6_ProductsMicroservice

---
+++ Section6_ProductsMicroservice/src/test/java/com/dypham/products/ProductsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dypham.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
